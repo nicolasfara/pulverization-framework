d7b5541ec44c671e57bcdbabe4049332e6e127a2
Nicolas Farabegoli <nicolas.farabegoli@gmail.com>
2022-12-20 09:04:04 +0100

    feat: implement context creation on native side

--- platform/src/nativeMain/kotlin/it/nicolasfarabegoli/pulverization/runtime/context/CreateContext.kt
+++ platform/src/nativeMain/kotlin/it/nicolasfarabegoli/pulverization/runtime/context/CreateContext.kt
@@ -1,7 +1,30 @@
 package it.nicolasfarabegoli.pulverization.runtime.context
 
 import it.nicolasfarabegoli.pulverization.component.Context
+import kotlinx.cinterop.addressOf
+import kotlinx.cinterop.usePinned
+import platform.posix.fclose
+import platform.posix.fgets
+import platform.posix.fopen
 
 internal actual suspend fun createContext(configFilePath: String): Context {
-    TODO("Not yet implemented")
+    val filePtr = fopen(configFilePath, "r") ?: error("Unable to open the file $configFilePath")
+    val contentRaw = ByteArray(255)
+    contentRaw.usePinned {
+        fgets(it.addressOf(0), 255, filePtr)
+    }
+    fclose(filePtr)
+    val content = contentRaw.decodeToString()
+    getKey("DEVICE_ID", content)?.let {
+        return object : Context {
+            override val deviceID: String = it
+        }
+    } ?: error("Unable to get the `DEVICE_ID` from config file")
+}
+
+internal fun getKey(key: String, content: String): String? {
+    val regex = "(.+)=(.+)".toRegex()
+    val matched = content.lines().mapNotNull { regex.find(it) }.map { it.destructured }
+        .firstOrNull { (k, _) -> k == key }
+    return matched?.let { (_, c) -> c }
 }
