59dc177cbedcd006dcca60c4a8f52f7c3e4d489d
Nicolas Farabegoli <nicolas.farabegoli@gmail.com>
2022-12-20 14:37:42 +0100

    chore: reorder projects

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -3,7 +3,7 @@ rootProject.name = "pulverization-framework"
 include(":core")
 include(":platform")
 include(":rabbitmq-platform")
+include(":rabbitmq-platform:native")
 // include(":examples:example-01")
 // include(":examples:example-02")
 include(":examples:example-03")
-include(":rabbitmq-platform:native")
